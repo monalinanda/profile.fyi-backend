2fe9e277faea3de25c744cc2e2616925a9c418a1
monalinanda <monalinanda95@gmail.com>
2024-08-17 16:45:21 +0530

    change code in index

--- index.js
+++ index.js
@@ -83,6 +83,7 @@ app.delete("/cart/:id", async (req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
